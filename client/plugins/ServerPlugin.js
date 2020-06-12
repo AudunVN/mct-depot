20fd367d740a29e79513ebd5507b3cea3a3a7065
Space <space@471.no>
2020-06-12 19:04:28 +0200

    Removed old Open MCT dictionary plugin

--- client/plugins/ServerPlugin.js
+++ client/plugins/ServerPlugin.js
@@ -1,5 +1,65 @@
 "use strict";
 
+function getConfig() {
+    return Promise.resolve(openmct.serverPlugin.config);
+}
+
+let objectProvider = {
+    get: function (identifier) {
+        return getConfig().then(function (config) {
+            if (identifier.key.indexOf(".rootfolder") != -1) {
+                let def = config.defs.filter(function (d) {
+                    return d.type === identifier.key.split(".")[0];
+                })[0];
+
+                return {
+                    identifier: identifier,
+                    name: def.name,
+                    type: 'folder',
+                    location: 'ROOT'
+                };
+            } else {
+                let measurement = config.measurements.filter(function (m) {
+                    return m.key === identifier.key;
+                })[0];
+
+                if (typeof measurement === "undefined") {
+                    console.log("Could not find measurement for " + identifier.key);
+                }
+
+                let telemetryObject = {
+                    identifier: identifier,
+                    name: measurement.name,
+                    type: 'omctserver.telemetry',
+                    telemetry: {
+                        values: measurement.telemetry.values
+                    },
+                    location: 'omctserver.taxonomy:spacecraft'
+                };
+
+                return telemetryObject;
+            }
+        });
+    }
+};
+
+let compositionProvider = {
+    appliesTo: function (domainObject) {
+        return domainObject.identifier.namespace === 'omctserver.taxonomy' &&
+               domainObject.type === 'folder';
+    },
+    load: function (domainObject) {
+        return getConfig().then(function (config) {
+            return config.measurements.map(function (m) {
+                return {
+                    namespace: 'omctserver.taxonomy',
+                    key: m.key
+                };
+            });
+        });
+    }
+};
+
 class ServerPlugin {
     constructor(config, metadata) {
         this.config = config;
@@ -8,7 +68,25 @@ class ServerPlugin {
 
     installer(openmct) {
         let config = openmct.serverPlugin.config;
-        let metadata = openmct.serverPlugin.metadata;
+
+        for (let i = 0; i < config.defs.length; i++) {
+            let def = config.defs[i];
+
+            openmct.objects.addRoot({
+                namespace: 'omctserver.taxonomy',
+                key: def.type + '.rootfolder'
+            });
+        }
+
+        openmct.objects.addProvider('omctserver.taxonomy', objectProvider);
+
+        openmct.composition.addProvider(compositionProvider);
+
+        openmct.types.addType('omctserver.telemetry', {
+            name: 'Telemetry Point',
+            description: 'Open MCT server telemetry point',
+            cssClass: 'icon-telemetry'
+        });
 
         console.log("Open MCT server plugin installed");
     }
