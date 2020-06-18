5f6c6511f6f12f70c44b9f7a73ab7c1a1b24b685
Space <space@471.no>
2020-06-18 02:55:35 +0200

    Added starting point for RealtimeClient and HistoryClient tests

--- client/plugins/HistoryClient.test.js
+++ client/plugins/HistoryClient.test.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const HistoryClient = require('./HistoryClient');
+const historyClient = new HistoryClient();
+
+test('plugin has installer function', async () => {
+    expect(typeof historyClient.installer).toBe("function");
+});
--- client/plugins/RealtimeClient.test.js
+++ client/plugins/RealtimeClient.test.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const RealtimeClient = require('./RealtimeClient');
+const realtimeClient = new RealtimeClient();
+
+test('plugin has installer function', async () => {
+    expect(typeof realtimeClient.installer).toBe("function");
+});
